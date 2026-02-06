b3f9f03e34f5ab8568384042e2a6673fe4af24b8
BrandonLeow123 <Brandon_Leow_from.TP@mom.gov.sg>
2026-02-06 16:06:24 +0800

    Create Useful_logic.py

---
+++ Useful_logic.py
@@ -0,0 +1,9 @@
+# Derived-column logic
+"""
+1. Read one or more existing columns
+2. Compute a new value
+3. Insert new column(s)
+4. Fill values row-by-row
+5. Avoid breaking column indices
+"""
+
